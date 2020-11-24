b633d5ce9d36130247308358a88b3cab731de5e8
Luke Liu <lukeliu@berkeley.edu>
2020-11-24 17:51:24 -0500

    input

--- input_generator.py
+++ input_generator.py
@@ -20,7 +20,38 @@ def graph_generator(num_vertices):
 
     return G
 
-write_input_file(graph_generator(10), 40, '10.in')
+def new_graph_generator(num_vertices):
+    G = nx.Graph()
+    G.add_nodes_from(range(num_vertices))
+    G.add_edge(0, 1, happiness=11, stress=6)
+    G.add_edge(0, 2, happiness=13, stress=7)
+    G.add_edge(1, 2, happiness=13, stress=7)
+
+    G.add_edge(2, 3, happiness=15, stress=7)
+
+    G.add_edge(3, 4, happiness=9, stress=5)
+    G.add_edge(3, 5, happiness=15, stress=8)
+    G.add_edge(4, 5, happiness=13, stress=7)
+
+    G.add_edge(5, 6, happiness=15, stress=7)
+
+    G.add_edge(6, 7, happiness=7, stress=4)
+    G.add_edge(6, 8, happiness=5, stress=3)
+    G.add_edge(6, 9, happiness=9, stress=5)
+    G.add_edge(7, 8, happiness=9, stress=5)
+    G.add_edge(7, 9, happiness=1, stress=1)
+    G.add_edge(8, 9, happiness=3, stress=2)
+
+    G.add_edge(9, 0, happiness=15, stress=7)
+
+    for i in range(num_vertices):
+        for j in range(i+1, num_vertices):
+            if not G.has_edge(i, j):
+                G.add_edge(i, j, happiness=round(random(), 3), stress=round(random() * 5 + 5, 3))
+
+    return G
+
+write_input_file(new_graph_generator(10), 40, '10.in')
 
 def n_generator(num_vertices, filename):
     text_file = open(filename, "wt")
