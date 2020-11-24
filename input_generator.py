035879893ee0937acf9f50b737182a31da840d8e
Luke Liu <lukeliu@berkeley.edu>
2020-11-24 02:16:27 -0500

    input generator

--- input_generator.py
+++ input_generator.py
@@ -1,6 +1,26 @@
-# import networkx as nx
+import networkx as nx
 from random import seed
 from random import random
+from parse import write_input_file
+
+def graph_generator(num_vertices):
+    G = nx.Graph()
+    G.add_nodes_from(range(num_vertices))
+    G.add_edge(0, 1, happiness=15, stress=5)
+    G.add_edge(0, 2, happiness=12, stress=4.5)
+    G.add_edge(0, 3, happiness=9, stress=3.5)
+    G.add_edge(1, 2, happiness=0, stress=20)
+    G.add_edge(1, 3, happiness=10.5, stress=4)
+    G.add_edge(2, 3, happiness=11, stress=5)
+
+    for i in range(num_vertices):
+        for j in range(i+1, num_vertices):
+            if i > 2 or j > 3:
+                G.add_edge(i, j, happiness=round(random() * 10, 3), stress=round(random() * 5, 3))
+
+    return G
+
+write_input_file(graph_generator(10), 40, '10.in')
 
 def n_generator(num_vertices, filename):
     text_file = open(filename, "wt")
@@ -14,5 +34,3 @@ def n_generator(num_vertices, filename):
 
     text_file.close()
 
-n_generator(20, "input_20.txt")
-n_generator(50, "input_50.txt")
