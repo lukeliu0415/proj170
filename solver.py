c31a21b402f41ed76f1bc82b3769464bbcd7c446
Luke Liu <lukeliu@berkeley.edu>
2020-12-07 00:28:52 -0500

    small and medium outputs

--- solver.py
+++ solver.py
@@ -36,7 +36,6 @@ def solve_naive(G, s):
     numRooms = 0
 
     for n, p in enumerate(partition(list(range(10))), 1):
-        print(n)
         isValid = True
         for room in p:
             if calculate_stress_for_room(room, G) > s / len(p):
@@ -168,18 +167,18 @@ def solve(G, s):
 
 # Here's an example of how to run your solver.
 # Usage: python3 solver.py test.in
-if __name__ == '__main__':
-    # assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G, s = read_input_file(path + ".in")
-    if len(sys.argv) > 2 and sys.argv[2] == "g":
-        D, k = solve_greedy(G, s)
-    else:
-        D, k = solve(G, s)
-    assert is_valid_solution(D, G, s, k)
-    print(D)
-    print(k)
-    print("Total Happiness: {}".format(calculate_happiness(D, G)))
+# if __name__ == '__main__':
+#     # assert len(sys.argv) == 2
+#     path = sys.argv[1]
+#     G, s = read_input_file(path + ".in")
+#     if len(sys.argv) > 2 and sys.argv[2] == "g":
+#         D, k = solve_greedy(G, s)
+#     else:
+#         D, k = solve(G, s)
+#     assert is_valid_solution(D, G, s, k)
+#     print(D)
+#     print(k)
+#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
 #     write_output_file(D, 'outputs/test.out')
 
 #     D_staff = read_output_file(path+".out", G, s)
@@ -187,12 +186,13 @@ if __name__ == '__main__':
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
-# if __name__ == '__main__':
-#     inputs = glob.glob('inputs/*')
-#     for input_path in inputs:
-#         output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G, s = read_input_file(input_path)
-#         D, k = solve(G, s)
-#         assert is_valid_solution(D, G, s, k)
-#         happiness = calculate_happiness(D, G)
-#         write_output_file(D, output_path)
+if __name__ == '__main__':
+    inputs = glob.glob('inputs/medium-*')
+    for input_path in inputs:
+        output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
+        G, s = read_input_file(input_path)
+        D, k = solve(G, s)
+        assert is_valid_solution(D, G, s, k)
+        happiness = calculate_happiness(D, G)
+        write_output_file(D, output_path)
+        print(input_path)
